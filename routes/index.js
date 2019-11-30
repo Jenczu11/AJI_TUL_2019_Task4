da35501f373379fe3f5a9c16c40ac6b5ebb80090
Bartłomiej Jencz <bartek.jencz@wp.pl>
2019-11-30 17:36:56 +0100

    Added productController and routs

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@
 const express = require('express');
 const router = express.Router();
 const IndexController = require('../controllers/IndexController')
-
+const ProductsController = require('../controllers/ProductsController');
 // zdefiniowanie odpowiedzi dla "strony głównej"
 router.get('/', IndexController.home);
+router.get('/products',ProductsController.getAll);
+router.get('/products/:id',ProductsController.getById);
+router.post('/products',ProductsController.store);
+router.put('/products/:id',ProductsController.updateById);
 
 module.exports = router;
