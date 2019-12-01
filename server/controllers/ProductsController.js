57846453b6221f33b2e987a7b84a06240549d5a2
Bartłomiej Jencz <bartek.jencz@wp.pl>
2019-12-01 19:32:09 +0100

    Products Controller Done

--- server/controllers/ProductsController.js
+++ server/controllers/ProductsController.js
@@ -1,78 +1,85 @@
 // const products = require('../data/ProductData');
-const Product = require('../models/product');
-const _ = require('underscore');
+const Product = require("../models/product");
+const _ = require("underscore");
 
-exports.getAll = (req,res) => {
-    Product.getAll().then(
-        function(allProducts) {
-            res.json(allProducts);
-        }
-    )  
-};
-exports.getById = (req,res) => {
-    Product.getById(req.params.id).then((product) =>{
-        res.json(product);
+exports.getAll = (req, res) => {
+    console.log("Products getAll");
+  Product.getAll()
+    .then(function(allProducts) {
+      res.json(allProducts);
     })
-
+    .catch(err => {
+      res.status(500).json({ error: err.message });
+    });
+};
+exports.getById = (req, res) => {
+    console.log("Products getById");
+  Product.getById(req.params.id)
+    .then(product => {
+      res.json(product);
+    })
+    .catch(err => {
+      res.status(404).json({ error: "Product not found" });
+    });
 };
 
-exports.store = (req,res) =>
-{
-    let inp = req.body;
-    if(inp.cena_jednostkowa<0 || inp.waga_jednostkowa<0)
-    {
-        res.status(400).json({
-            'error': 'ujemna cena/waga'
-        })
+exports.store = (req, res) => {
+    console.log("Products store");
+    try {
+        checkRequest(req,0)
+        const newProduct = Product.create({
+            nazwa: req.body.nazwa,
+            opis: req.body.opis,
+            cena_jednostkowa: req.body.cena_jednostkowa,
+            waga_jednostkowa: req.body.waga_jednostkowa,
+            kategoria_towaru: req.body.kategoria_towaru
+          })
+            .then(() => {
+              res.json({
+                status: "saved!",
+                product: newProduct
+              });
+            }).catch((err) => {
+                res.status(500).json({error: err.message})
+            })
     }
-    else
-    if(inp.cena_jednostkowa==0 || inp.waga_jednostkowa==0)
-    {
-        res.status(400).json({
-            'error': 'zerowa cena/waga'
-        })
-    }else
-    if(inp.opis === "" || inp.nazwa==="")
-    {
-        res.status(400).json({
-            'error': 'pusty opis/nazwa'
-        })
-    }else
-    {
-    const newProduct = Product.create( {
-        'nazwa': inp.nazwa,
-        'opis': inp.opis,
-        'cena_jednostkowa': inp.cena_jednostkowa,
-        'waga_jednostkowa': inp.waga_jednostkowa,
-        'kategoria_towaru': inp.kategoria_towaru
-    }).then( () => {
-        res.json({
-            'status':'saved!',
-            'product': newProduct,
-        });
-    })
-}
-    // products.push(newProduct);
+    catch (error) {
+        res.status(500).json({error: error})
+    }
     
+   
 };
-exports.updateById = (req,res) => {
-    let listProducts;
-    Product.getAll().then(
-        function(allProducts) {
-            listProducts=allProducts;
-        }
-    ) 
-    console.log(listProducts);
-
-   Product.update(req.body.product).then(
-    function(product) {
+exports.updateById = (req, res) => {
+    console.log("Products updateById");
+  try {
+      checkRequest(req,1)
+    Product.update(req.body.product)
+      .then(function(product) {
         res.json(product);
-    }
-)    
-//     const currentProduct = _.find(products, (product) => {return product.id == req.params.id});
-//     currentProduct.name = req.body.name;
-//     currentProduct.description = req.body.description;
-//     currentProduct.price = req.body.price;
-//     currentProduct.amount = req.body.amount;
-//     res.json({'updatedProduct': currentProduct});
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
+      });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+function checkRequest(req,call) {
+    let input = req.body;
+    if(call==1) input = req.body.product
+    if (input.nazwa == "" ||
+    input.opis == "" ||
+    input.cena_jednostkowa == "" ||
+    input.waga_jednostkowa == "" ||
+    input.kategoria == "")
+{
+    throw "Kazde z pul musi miec jakas wartosc";
+}
+
+if (isNaN(parseFloat(input.cena_jednostkowa)) || isNaN(parseFloat(input.waga_jednostkowa))) {
+    throw "Cena i waga musza byc liczba";
+}
+if ((input.cena_jednostkowa<=0)||(input.waga_jednostkowa<=0))
+throw "Cena i waga nie moga byc ujemne"
 }
